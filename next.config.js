bd30c8ec65c37008ffcf3905328d2acb8037708d
Carlos <karlosarr@gmail.com>
2022-10-12 12:31:27 -0600

    page realm

--- next.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    images: {
-        domains: ["m.media-amazon.com"],
-    },
-};
--- pages/api/status.js
+++
@@ -1,3 +0,0 @@
-export default function getStatus(req, res) {
-    res.status(200).json({ name: "MongoFlix" });
-}
